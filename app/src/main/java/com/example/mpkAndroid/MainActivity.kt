98f1670963ee2a8b7556ea20e35abd584397b352
Paweł Gryglewicz <pawel.gryglewicz09@gmail.com>
2023-03-25 21:12:18 +0100

    Add view with google map

--- app/src/main/java/com/example/mpkAndroid/MainActivity.kt
+++ app/src/main/java/com/example/mpkAndroid/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.mpkAndroid.ui.MapScreen
 import com.example.mpkAndroid.ui.theme.MpkAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MapScreen()
                 }
             }
         }
